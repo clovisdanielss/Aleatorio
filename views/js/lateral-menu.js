33fb8558f8b6167094df5585bc09a021568cdd5c
clovisdanielss@gmail.com <clovisdanielss@gmail.com>
2018-01-29 17:26:39 -0300

    adicionada funcao para limpar modal

--- views/js/lateral-menu.js
+++ views/js/lateral-menu.js
@@ -1,6 +1,8 @@
 function openNav() {
 	closeNav()
 	setTimeout(function(){
+		//falta verificar outra alternativa para mobile.
+		//checar o tamanho da tela e fazer outra abordagem é uma opção.
 		document.getElementById("mySidenav").style.width = "20%";
 		var block = document.getElementById("main-content");
 		block.style.width = "80%";
--- views/js/user-table.js
+++ views/js/user-table.js
@@ -1,21 +1,6 @@
-window.onload = function(){
-    $('tr').on('click', function(){
-        var id = $(this).attr('id');
-        $('#exampleModal').modal('toggle')
-        //$('#textField').attr('value', id);
-    })
-    $('.dropdown-item').on ('click', function(){
-        var newText = $(this).text();
-        $('#dropdownMenuButton').text($(this).text());
-        if(newText == 'CherryTrack'){
-            $('#ops').show();
-        }else{
-            $('#ops').hide();
-        }
-    })
+function selectPermission() {
     $('.mymodal.list-group-item-action.list-group-item').on('click',function(){
         var selected = $(this).attr('ligado');
-        console.log(selected);
         if(!selected || selected=='false'){
             $(this).attr('ligado', 'true');
             $(this).addClass('active');
@@ -24,6 +9,41 @@ window.onload = function(){
             $(this).attr('ligado', 'false');
             $(this).removeClass('active');
         }
-    })
+    });
+}
 
+function selectSystem(){
+    $('.dropdown-item').on ('click', function(){
+        var newText = $(this).text();
+        $('#dropdownMenuButton').text($(this).text());
+        if(newText == 'CherryTrack'){
+            $('#ops').show();
+        }else{
+            $('#ops').hide();
+        }
+    });
+}
+
+function openModal(){
+    $('tr').on('click', function(){
+        var id = $(this).attr('id');
+        $('#exampleModal').modal('toggle')
+    });
+}
+
+function cleanSelected(){
+    $('.btn-clean').on('click', function(){
+        var col = $('.active');
+        col.each(function(item){
+            $(col[item]).attr('ligado', 'false');
+            $(col[item]).removeClass('active');
+        });
+    });
+}
+
+window.onload = function(){
+    openModal();
+    selectSystem();
+    selectPermission();
+    cleanSelected();
 }
