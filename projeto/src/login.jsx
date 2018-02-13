eceabf6b46bb1dc195d79b5a8940fac91455c673
clovisdanielss@gmail.com <clovisdanielss@gmail.com>
2018-02-13 11:51:08 -0300

    Tela de login responsiva

--- projeto/src/login.jsx
+++ projeto/src/login.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import {Grid, Col, Row} from 'react-bootstrap';
 import {Card, CardHeader, CardBody, Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
+//As classes devem ser do bootstrap 4. (Não possui tags para Grid,Col,Row)
+//As tags(Grid,Col,Row) são do bootstrap 3.
+
 const contentNode = document.getElementById('content');
 
 class Login extends React.Component{
     render(){
         return(
-            <Grid>
+            <Grid style={{marginTop:"12.5%"}}>
                 <Row>
-                    <Col sm={4} xs={8} className="offset-sm-4">
+                    <Col lg={4} md={6} sm={8} className="offset-sm-2 offset-md-3 offset-lg-4">
                         <Card>
                             <CardHeader>
                                 Login
@@ -26,7 +29,7 @@ class Login extends React.Component{
                                         <Input id="password" type="password"></Input>
                                     </FormGroup>
                                     <Row>
-                                        <Col className="offset-sm-5">
+                                        <Col sm={4} className="offset-sm-4 offset-4">
                                             <Button color="primary">Entrar</Button>
                                         </Col>
                                     </Row>
